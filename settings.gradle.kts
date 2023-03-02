10b87383a22ab83fee08bc44c123c55474e4184f
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-03-02 16:32:16 +0330

    migrate Settings Build.gradle to kotlin DSL.

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+   }
+rootProject.name = "Weather"
+include(":app")
+include(":core:database")
+include(":feature:forecast")
+include(":core:model")
+include(":core:network")
+include(":core:repository")
+include(":core:design")
+include(":feature:search")
+include(":sync:work")
