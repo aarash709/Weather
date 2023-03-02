de7bd1267e7d76c96ad440d35175e3ba99be2924
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-03-02 15:49:42 +0330

    migrate Model Build.gradle to kotlin DSL.

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id 'java-library'
-    id 'org.jetbrains.kotlin.jvm'
-    id 'org.jetbrains.kotlin.plugin.serialization'
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 java {
