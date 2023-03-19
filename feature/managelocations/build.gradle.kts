d910aa184938c0bd2d1f26c39067620bd9631182
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-03-19 13:06:42 +0330

    Add manageLocations module and dependencies.

---
+++ feature/managelocations/build.gradle.kts
@@ -0,0 +1,82 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("org.jetbrains.kotlin.plugin.serialization")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.weather.feature.managelocations"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(project(":core:repository"))
+    implementation(project(":core:design"))
+    implementation(project(":feature:forecast"))
+    implementation(project(":core:model"))
+//Compose
+    implementation(platform("androidx.compose:compose-bom:${rootProject.extra.get("compose_bom_version")}"))
+    implementation("androidx.compose.ui:ui:")
+    implementation("androidx.compose.material:material:")
+    implementation("androidx.compose.material:material-icons-extended:")
+    implementation("androidx.compose.ui:ui-tooling-preview:")
+    debugImplementation ("androidx.compose.ui:ui-tooling:")
+    implementation("androidx.compose.runtime:runtime:")
+    implementation("androidx.compose.runtime:runtime-livedata:")
+    implementation("androidx.compose.foundation:foundation:")
+    implementation("androidx.compose.animation:animation:")
+    //navigation
+    implementation("androidx.navigation:navigation-compose:${rootProject.extra.get("compose_nav_version")}")
+    implementation("com.google.accompanist:accompanist-navigation-animation:${rootProject.extra.get("accompanist_version")}")
+    //LifeCycle Components
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra.get("lifecycle_version")}")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0-beta01") //experimental
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra.get("lifecycle_version")}")
+    //Hilt
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("com.google.dagger:hilt-android:2.44.2")
+    kapt("com.google.dagger:hilt-compiler:2.44.2")
+    //serializer
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    //datastore
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    //Timber Logging
+    implementation("com.jakewharton.timber:timber:5.0.1")
+    //
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ feature/managelocations/src/main/java/com/weather/feature/managelocations/ManageLocationsViewModel.kt
@@ -0,0 +1,109 @@
+package com.weather.feature.managelocations
+
+import android.app.Application
+import android.content.Context
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.weather.core.repository.WeatherRepository
+import com.weather.feature.forecast.dataStore
+import com.weather.model.Coordinate
+import com.weather.model.ManageLocationsData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class ManageLocationsViewModel @Inject constructor(
+    private val weatherRepository: WeatherRepository,
+    private val context: Application,
+) : ViewModel() {
+
+    private val hapticFeedback = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val locationsState = weatherRepository.getAllWeatherLocations()
+        .combine(getFavoriteCityCoordinate()) { weatherList, favoriteCoordinate ->
+            val locationData = weatherList.map {
+                val isFavorite = it.locationName == favoriteCoordinate.cityName
+                it.copy(isFavorite = isFavorite)
+            }
+            LocationsUIState.Success(locationData)
+        }
+        .flowOn(Dispatchers.IO)
+        .stateIn(
+            viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = LocationsUIState.Loading
+        )
+
+    @Deprecated("use new method")
+    fun saveFavoriteCity(cityName: String) {
+        if (cityName.isEmpty()) {
+            Timber.e("city name is empty")
+            return
+        }
+        viewModelScope.launch(Dispatchers.IO) {
+            context.dataStore.edit { preference ->
+                preference[DataStoreKeys.WeatherDataStore.FAVORITE_CITY_String_Key] = cityName
+            }
+        }
+    }
+
+    fun saveFavoriteCityCoordinate(coordinate: Coordinate) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val coordinateString = Json.encodeToString(coordinate)
+            context.dataStore.edit { preference ->
+                preference[DataStoreKeys.WeatherDataStore.FAVORITE_CITY_Coordinate_String_Key] =
+                    coordinateString
+            }
+        }
+    }
+
+    @ExperimentalCoroutinesApi
+    private fun getFavoriteCityCoordinate(): Flow<Coordinate> {
+        return context.dataStore.data.map { preferences ->
+            val string =
+                preferences[DataStoreKeys.WeatherDataStore.FAVORITE_CITY_Coordinate_String_Key]
+                    ?: ""
+            Timber.e(string)
+            string
+        }.map { coordinate ->
+            Json.decodeFromString<Coordinate>(coordinate)
+        }
+    }
+
+    fun deleteWeatherByCityName(cityName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            weatherRepository.deleteWeatherByCityName(cityName = cityName)
+        }
+        hapticFeedback.cancel()
+        hapticFeedback.vibrate(10)
+
+    }
+
+}
+
+sealed interface LocationsUIState {
+    object Loading : LocationsUIState
+    data class Success(val data: List<ManageLocationsData>) : LocationsUIState
+}
+object DataStoreKeys {
+    object WeatherDataStore {
+        val FAVORITE_CITY_String_Key = stringPreferencesKey("favoriteCity")
+        val FAVORITE_CITY_Coordinate_String_Key = stringPreferencesKey("favoriteCityCoordinate")
+    }
+}
+
