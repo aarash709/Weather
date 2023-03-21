2cbeb48b07c251bccd1bc5501602aba8812c4cc5
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-03-21 16:58:56 +0330

    Added datastore Hilt DI.

--- core/datastore/src/main/java/com/eweather/core/datastore/DatastoreModule.kt
+++ core/datastore/src/main/java/com/eweather/core/datastore/DatastoreModule.kt
@@ -1,18 +1,26 @@
 package com.eweather.core.datastore
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+
+const val USER_CONFIG_PREFERENCE = "UserPreference"
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatastoreModule {
-
     @Provides
     @Singleton
-    fun providesDatastore() {
-
-    }
+    fun providesDatastore(@ApplicationContext context: Context): DataStore<Preferences> =
+        PreferenceDataStoreFactory.create {
+            context.preferencesDataStoreFile(USER_CONFIG_PREFERENCE)
+        }
 }
