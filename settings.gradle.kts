7fed8cb323f9fb3e0ff6264ed1a9d7e9788d8ac3
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-03-23 12:35:37 +0430

    add compose to version catalog.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,22 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
+    versionCatalogs {
+        create("libs"){
+            version("compose.bom", "2023.03.00")
+            version("compose-version", "1.4.0")
+            library("compose-bom","androidx.compose","compose.bom").versionRef("compose.bom")
+            library("compose-animation","androidx.compose.animation","animation").versionRef("compose-version")
+            library("compose-foundation","androidx.compose.foundation","foundation").versionRef("compose-version")
+            library("compose-foundation","androidx.compose.ui","ui").versionRef("compose-version")
+            library("compose-foundation","androidx.compose.ui","ui").versionRef("compose-version")
+            library("compose-foundation","androidx.compose.ui","ui-tooling").versionRef("compose-version")
+            library("compose-foundation","androidx.compose.runtime","runtime").versionRef("compose-version")
+            library("compose-foundation","androidx.compose.runtime","runtime-livedata").versionRef("compose-version")
+            library("compose-foundation","androidx.compose.material","material").versionRef("compose-version")
+            library("compose-foundation","androidx.compose.material","material-icons-extended").versionRef("compose-version")
+        }
+    }
    }
 rootProject.name = "Weather"
 include(":app")
