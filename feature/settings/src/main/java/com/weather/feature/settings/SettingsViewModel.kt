0cc033ea72bab1348aeb5648de879ffe45f20a39
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-03-25 10:56:27 +0430

    Adding settings methods.

--- feature/settings/src/main/java/com/weather/feature/settings/SettingsViewModel.kt
+++ feature/settings/src/main/java/com/weather/feature/settings/SettingsViewModel.kt
@@ -7,5 +7,18 @@ import javax.inject.Inject
 @HiltViewModel
 class SettingsViewModel @Inject constructor() : ViewModel() {
 
+    internal fun setTemperatureUnit(tempUnits: TempUnits) {
+    }
 
+    internal fun  setWindSpeedUnit(windSpeedUnits: WindSpeedUnits){
+    }
+}
+enum class WindSpeedUnits{
+    KM,
+    MS,
+    MPH,
+}
+enum class TempUnits{
+    C,
+    F,
 }
