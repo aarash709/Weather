c84f50f072ffb569a8326796fd8f18c91fbd539b
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-03-25 11:57:45 +0430

    added settings state class and necessary methods.

--- feature/settings/src/main/java/com/weather/feature/settings/SettingsViewModel.kt
+++ feature/settings/src/main/java/com/weather/feature/settings/SettingsViewModel.kt
@@ -1,24 +1,45 @@
 package com.weather.feature.settings
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.weather.core.repository.WeatherRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor() : ViewModel() {
+class SettingsViewModel @Inject constructor(private val repository: WeatherRepository) : ViewModel() {
 
-    internal fun setTemperatureUnit(tempUnits: TempUnits) {
+    internal fun setTemperatureUnit(tempUnits: TemperatureUnits) {
+        viewModelScope.launch {
+//            repository.setTemperatureUnit
+        }
     }
 
-    internal fun  setWindSpeedUnit(windSpeedUnits: WindSpeedUnits){
+    internal fun setWindSpeedUnit(windSpeedUnits: WindSpeedUnits) {
+        viewModelScope.launch {
+//            repository.setWindSpeedUnit
+        }
     }
 }
-enum class WindSpeedUnits{
+
+internal sealed class SettingsUIState() {
+    data class Success(val settingsData: SettingsData) : SettingsUIState()
+    object Loading : SettingsUIState()
+}
+
+internal data class SettingsData(
+    val windSpeedUnits: WindSpeedUnits,
+    val temperatureUnits: TemperatureUnits,
+)
+
+enum class WindSpeedUnits {
     KM,
     MS,
     MPH,
 }
-enum class TempUnits{
+
+enum class TemperatureUnits {
     C,
     F,
 }
