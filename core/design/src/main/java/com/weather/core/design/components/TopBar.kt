c4bc9e4ce9b0d5c66c9fafbf1e0780c78a57cf13
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-04-01 16:59:07 +0330

    Fix CustomTopBar padding.

--- core/design/src/main/java/com/weather/core/design/components/TopBar.kt
+++ core/design/src/main/java/com/weather/core/design/components/TopBar.kt
@@ -1,5 +1,7 @@
 package com.weather.core.design.components
 
+import android.content.res.Configuration
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -9,9 +11,12 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.weather.core.design.theme.WeatherTheme
 
 @Composable
 fun CustomTopBar(
@@ -21,10 +26,12 @@ fun CustomTopBar(
     elevation: Dp = 0.dp,
     onBackPressed: () -> Unit,
 ) {
+    val contentPadding = PaddingValues(horizontal = 0.dp)
     TopAppBar(
         modifier = modifier,
         backgroundColor = backgroundColor,
-        elevation = elevation
+        elevation = elevation,
+        contentPadding = contentPadding,
     ) {
         CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
             TopBarSimple(
@@ -37,7 +44,7 @@ fun CustomTopBar(
 
 @Composable
 fun TopBarSimple(
-    text : String,
+    text: String,
     onBackPressed: () -> Unit,
 ) {
     Row(
@@ -46,17 +53,24 @@ fun TopBarSimple(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Start
     ) {
-        IconButton(onClick = { onBackPressed() }) {
-            Icon(
-                modifier = Modifier,
-                imageVector = Icons.Default.ArrowBack,
-                contentDescription = "Back Icon"
-            )
-        }
-        Spacer(modifier = Modifier.width(16.dp))
+        Icon(
+            modifier = Modifier.clickable() { onBackPressed() },
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = "Back Icon"
+        )
+        Spacer(modifier = Modifier.width(32.dp))
         Text(
             text = text,
             fontSize = 18.sp
         )
+    }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+private fun TopBarPreview() {
+    WeatherTheme() {
+        CustomTopBar(text = "Text") {}
     }
 }
