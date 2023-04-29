5b8a80166054d4c7c143d31d5c7c576828856ea4
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-04-29 13:46:17 +0330

    WIP build type configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,13 @@ android {
 
     buildTypes {
         release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+        debug {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+
         }
     }
     compileOptions {
