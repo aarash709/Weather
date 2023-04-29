d0ea0a2a27b8c76b3fc96f7ebcd87b0c379adbbe
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-04-29 12:03:57 +0330

    Add placeholder animation to search list items.

--- core/model/src/main/java/com/weather/model/geocode/GeoSearchItem.kt
+++ core/model/src/main/java/com/weather/model/geocode/GeoSearchItem.kt
@@ -1,5 +1,18 @@
 package com.weather.model.geocode
 
+data class SavableSearchState(
+    val geoSearchItems: List<GeoSearchItem>,
+    val showPlaceholder: Boolean = true,
+) {
+    companion object {
+        val empty = SavableSearchState(
+            geoSearchItems = GeoSearchItem.empty,
+            showPlaceholder = true
+        )
+
+    }
+}
+
 data class GeoSearchItem(
     val country: String? = null,
     val lat: Double? = null,
@@ -7,4 +20,17 @@ data class GeoSearchItem(
     val lon: Double? = null,
     val name: String? = null,
     val state: String? = null,
-)
+) {
+    companion object {
+        val empty = List(4) {
+            GeoSearchItem(
+                country = null,
+                lat = null,
+                local_names = null,
+                lon = null,
+                name = null,
+                state = null,
+            )
+        }
+    }
+}
