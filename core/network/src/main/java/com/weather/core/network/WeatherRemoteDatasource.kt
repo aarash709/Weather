5b1d8423ada24dddfd9eb138a4113e2fd8e3f4f5
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-05-06 13:31:57 +0330

    Added Ktor Client.

--- core/network/src/main/java/com/weather/core/network/WeatherRemoteDatasource.kt
+++ core/network/src/main/java/com/weather/core/network/WeatherRemoteDatasource.kt
@@ -1,7 +1,10 @@
 package com.weather.core.network
 
+import com.weather.core.network.BuildConfig.API_KEY
+import com.weather.core.network.ktor.KtorApiService
 import com.weather.core.network.model.weather.NetworkOneCall
 import com.weather.core.network.retrofit.RetrofitApiService
+import com.weather.core.network.util.APIKey
 import com.weather.model.Coordinate
 import com.weather.model.Resource
 import com.weather.model.geocode.GeoSearchItem
@@ -10,14 +13,17 @@ import timber.log.Timber
 import java.io.IOException
 
 class WeatherRemoteDatasource(
-    private val remoteApi: RetrofitApiService,
+//    private val remoteApi: RetrofitApiService,
+    private val ktorApi: KtorApiService,
+
 ) {
     suspend fun getRemoteData(coordinates: Coordinate, exclude: String): Resource<NetworkOneCall> {
         return try {
-            val data = remoteApi.getOneCall(
+            val data = ktorApi.getOneCall(
                 lat = coordinates.latitude,
                 lon = coordinates.longitude,
-                exclude = exclude
+                exclude = exclude,
+                API_KEY
             )
 //            Timber.e(data.timezone)
             Resource.Success(data = data)
@@ -30,7 +36,7 @@ class WeatherRemoteDatasource(
 
     suspend fun directGeocode(cityName: String): List<GeoSearchItem> {
         return try {
-            remoteApi.getGeoSearch(location = cityName, limit = "5").map {
+            ktorApi.getGeoSearch(location = cityName, limit = "5", API_KEY).map {
                 it.toGeoSearchItem()
             }
         } catch (e: Exception) {
