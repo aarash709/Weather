a3e65337a4c127b00d4d2674812143079598ad6e
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-06-01 12:48:20 +0330

    Added build-logic module for shared logic.

---
+++ build-logic/conventionplugins/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    `kotlin-dsl`
+//    id("java-library")
+//    id("org.jetbrains.kotlin.jvm")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
---
+++ build-logic/conventionplugins/src/main/java/com/experiment/buildlogic/conventionplugins/ComposeConventionPlugin.kt
@@ -0,0 +1,10 @@
+package com.experiment.buildlogic.conventionplugins
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ComposeConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        TODO("Not yet implemented")
+    }
+}
