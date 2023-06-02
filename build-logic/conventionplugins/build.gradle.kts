09418e970573f10cafcd235f6d205798673d7c6e
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-06-02 15:12:01 +0330

    compose build configuration.

--- build-logic/conventionplugins/build.gradle.kts
+++ build-logic/conventionplugins/build.gradle.kts
@@ -4,7 +4,13 @@ plugins {
 //    id("org.jetbrains.kotlin.jvm")
 }
 
+
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies{
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
 }
--- build-logic/conventionplugins/src/main/java/com/experiment/buildlogic/conventionplugins/ComposeConventionPlugin.kt
+++ build-logic/conventionplugins/src/main/java/com/experiment/buildlogic/conventionplugins/ComposeConventionPlugin.kt
@@ -1,10 +1,37 @@
 package com.experiment.buildlogic.conventionplugins
 
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 class ComposeConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        TODO("Not yet implemented")
+        target.extensions.getByType<ApplicationExtension>()
+
+    }
+}
+
+internal fun Project.composeBuildConfiguration(
+    commonExtension: CommonExtension<*, *, *, *>,
+) {
+    val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+    commonExtension.apply {
+        buildFeatures{
+            compose = true
+        }
+        composeOptions{
+            kotlinCompilerExtensionVersion = libs.findVersion("androidxComposeCompiler").get().toString()
+        }
+
+        dependencies {
+            val composeBom = libs.findLibrary("compose-bom")
+            add("implementation",platform(composeBom))
+            add("androidTestImplementation",platform(composeBom))
+        }
     }
 }
