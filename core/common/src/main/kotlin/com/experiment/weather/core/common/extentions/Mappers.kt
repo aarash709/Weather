bd08668d86fc12866a5b9239df186dbbb226124f
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-12-16 11:51:04 +0330

    fix daily time calculator

--- core/common/src/main/kotlin/com/experiment/weather/core/common/extentions/Mappers.kt
+++ core/common/src/main/kotlin/com/experiment/weather/core/common/extentions/Mappers.kt
@@ -1,6 +1,5 @@
 package com.experiment.weather.core.common.extentions
 
-import android.util.Log
 import arrow.optics.copy
 import com.weather.model.Coordinate
 import com.weather.model.Current
@@ -28,13 +27,9 @@ import com.weather.model.wind_speed
 import java.text.SimpleDateFormat
 import java.time.Duration
 import java.time.Instant
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.Locale
-import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
-import kotlin.time.Duration.Companion.milliseconds
 
 fun Current.convertToUserSettings(
     temperature: TemperatureUnits?,
@@ -54,7 +49,7 @@ fun List<Daily>.convertToUserSettings(temperature: TemperatureUnits?): List<Dail
     return map { daily ->
         daily.copy {
             Daily.dew_point transform { dewPoint -> dewPoint.convertToUserTemperature(temperature) }
-            Daily.dt transform { dt -> unixMillisToHumanDate(dt.toLong(), "EEE") }
+            Daily.dt transform { dt -> unixMillisToHumanDate(dt.toLong(), DAILY_PATTERN) }
             Daily.dayTemp transform { dayTemp -> dayTemp.convertToUserTemperature(temperature) }
             Daily.nightTemp.transform { nightTemp -> nightTemp.convertToUserTemperature(temperature) }
         }
@@ -70,15 +65,7 @@ fun List<Hourly>.convertToUserSettings(temperature: TemperatureUnits?): List<Hou
                     temperature
                 )
             }
-            Hourly.dt transform { dt ->
-                val timeMillis = dt.toLong()
-                val diffrence = Duration.ofSeconds(timeMillis.minus(Instant.now().epochSecond)).toMinutes()
-                Log.e("mapper",diffrence.toString())
-                if (diffrence in 0 .. 60)
-                    "Now"
-                else
-                    unixMillisToHumanDate(timeMillis, "HH:mm")
-            }
+            Hourly.dt transform { hourlyTimeMillis -> calculateUIDailyTime(hourlyTimeMillis) }
             Hourly.temp transform { temp -> temp.convertToUserTemperature(temperature) }
         }
     }
@@ -92,15 +79,8 @@ fun WeatherData.convertToUserSettings(userSettings: SettingsData): WeatherData {
                 userSettings.windSpeedUnits
             )
         }
-        WeatherData.daily transform {
-
-            it.convertToUserSettings(userSettings.temperatureUnits)
-
-
-        }
-        WeatherData.hourly transform {
-            it.convertToUserSettings(userSettings.temperatureUnits)
-        }
+        WeatherData.daily transform { it.convertToUserSettings(userSettings.temperatureUnits) }
+        WeatherData.hourly transform { it.convertToUserSettings(userSettings.temperatureUnits) }
 
     }
 }
@@ -131,4 +111,14 @@ private fun unixMillisToHumanDate(unixTimeStamp: Long, pattern: String): String 
     val formatter = SimpleDateFormat(pattern, Locale.getDefault())
     val date = Date(unixTimeStamp * 1000) //to millisecond
     return formatter.format(date)
+}
+
+private fun calculateUIDailyTime(hourlyTimeMillis: String): String {
+    val timeMillis = hourlyTimeMillis.toLong()
+    val differenceInMinutes =
+        Duration.ofSeconds(timeMillis.minus(Instant.now().epochSecond)).toMinutes()
+    return if (differenceInMinutes in hourlyRangeThreshold)
+        NOW
+    else
+        unixMillisToHumanDate(timeMillis, HOURLY_PATTERN)
 }
---
+++ core/common/src/main/kotlin/com/experiment/weather/core/common/extentions/Values.kt
@@ -0,0 +1,7 @@
+package com.experiment.weather.core.common.extentions
+
+
+val hourlyRangeThreshold = -58..0
+const val NOW = "Now"
+const val HOURLY_PATTERN = "HH:mm"
+const val DAILY_PATTERN = "EEE"
