5748c15412dcc7f0febdcad41b236e5908fc1ba4
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-01-06 16:45:07 +0330

    code cleanup

--- feature/forecast/src/main/kotlin/com/weather/feature/forecast/components/HourlyForecast.kt
+++ feature/forecast/src/main/kotlin/com/weather/feature/forecast/components/HourlyForecast.kt
@@ -2,40 +2,22 @@ package com.weather.feature.forecast.components
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import co.yml.charts.axis.AxisData
-import co.yml.charts.common.model.Point
-import co.yml.charts.ui.linechart.LineChart
-import co.yml.charts.ui.linechart.model.IntersectionPoint
-import co.yml.charts.ui.linechart.model.Line
-import co.yml.charts.ui.linechart.model.LineChartData
-import co.yml.charts.ui.linechart.model.LinePlotData
-import co.yml.charts.ui.linechart.model.LineStyle
-import co.yml.charts.ui.linechart.model.SelectionHighlightPoint
-import co.yml.charts.ui.linechart.model.SelectionHighlightPopUp
-import co.yml.charts.ui.linechart.model.ShadowUnderLine
 import coil.compose.AsyncImage
 import com.weather.core.design.theme.WeatherTheme
 import com.weather.feature.forecast.components.hourlydata.HourlyStaticData
@@ -98,14 +80,6 @@ fun HourlyItem(
             text = "${item.temp.toFloat().roundToInt()}°",
             color = LocalContentColor.current.copy(alpha = 0.5f)
         )
-    }
-}
-
-@Preview
-@Composable
-private fun HourlyWithGraphTestPreview() {
-    WeatherTheme {
-        HourlyForecast(data = HourlyStaticData)
     }
 }
 
--- feature/forecast/src/main/kotlin/com/weather/feature/forecast/components/TempGraph.kt
+++ feature/forecast/src/main/kotlin/com/weather/feature/forecast/components/TempGraph.kt
@@ -1,6 +1,5 @@
 package com.weather.feature.forecast.components
 
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,40 +18,16 @@ import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import co.yml.charts.axis.AxisData
-import co.yml.charts.common.model.Point
-import co.yml.charts.ui.linechart.LineChart
-import co.yml.charts.ui.linechart.model.IntersectionPoint
-import co.yml.charts.ui.linechart.model.Line
-import co.yml.charts.ui.linechart.model.LineChartData
-import co.yml.charts.ui.linechart.model.LinePlotData
-import co.yml.charts.ui.linechart.model.LineStyle
-import co.yml.charts.ui.linechart.model.SelectionHighlightPoint
-import co.yml.charts.ui.linechart.model.SelectionHighlightPopUp
-import co.yml.charts.ui.linechart.model.ShadowUnderLine
-import com.weather.core.design.theme.WeatherTheme
 import com.weather.feature.forecast.components.hourlydata.HourlyStaticData
 import com.weather.model.Hourly
 import kotlin.math.roundToInt
 
 @Composable
-fun TempPoints(modifier: Modifier = Modifier) {
-    val pointsColor = MaterialTheme.colorScheme.onSurface
-    Spacer(modifier = modifier
-        .fillMaxWidth()
-        .drawWithCache {
-            val center = Offset(x = 0.0f, y = 0.0f)
-            onDrawBehind {
-                drawCircle(color = pointsColor, radius = 10f, center = center)
-            }
-        })
-}
-
-@Composable
 fun HourlyGraph(modifier: Modifier = Modifier, data: List<Hourly>) {
     val textColor = MaterialTheme.colorScheme.onSurface
+    val verticalLineColor = MaterialTheme.colorScheme.primary
     val textMeasurer = rememberTextMeasurer()
-//    val a = rememberVectorPainter(image = Icons.Outlined.Radio) //test
+
     Spacer(modifier = modifier then Modifier
         .padding(start = 0.dp, bottom = 0.dp)
         .fillMaxWidth()
@@ -65,36 +40,23 @@ fun HourlyGraph(modifier: Modifier = Modifier, data: List<Hourly>) {
             val tempRange = (maxTemp - minTemp).toFloat()
             val topOffset = 20.dp.toPx()
             val path = Path()
-            var previousTemp = height
+
             onDrawBehind {
-//                with(a){
-//                    draw(a.intrinsicSize) //test
-//                }
-                //horizontal line
-                drawLine(
-                    color = textColor,
-                    start = Offset(x = -130f, topOffset),
-                    end = Offset(x = width + 130, y = topOffset),
-                    pathEffect = PathEffect.dashPathEffect(
-                        floatArrayOf(10f, 10f),
-                        phase = 0f
-                    ),
-                )
+                var previousTemp = height
                 data.forEachIndexed { index, hourly ->
                     val temp = hourly.temp.toFloat()
                     val y = height - ((temp - minTemp) / tempRange)
                         .times(height.minus(topOffset))
                         .toFloat()
                     val x = width / (dataSize - 1)
-                    val xPerIndex = x * (index)
+                    val xPerIndex = x * index
                     val controlPoints1 = Offset(xPerIndex.minus(x / 2), previousTemp)
                     val controlPoints2 = Offset(xPerIndex.minus(x / 2), y)
                     drawText(
-                        textMeasurer.measure("${temp.roundToInt()}"),
+                        textMeasurer.measure("${temp.roundToInt()}°"),
                         color = textColor,
                         topLeft = Offset(xPerIndex - 15, y - 70)
                     )
-                    drawCircle(Color.Black, radius = 10f, center = Offset(xPerIndex, y))
                     drawLine(
                         color = Color.Red.copy(alpha = 0.5f),
                         start = Offset(x = xPerIndex, y),
@@ -106,15 +68,16 @@ fun HourlyGraph(modifier: Modifier = Modifier, data: List<Hourly>) {
                         ),
                     )
                     if (index == data.lastIndex) {
-                        path.lineTo(xPerIndex + 130, y = y)
+                        path.lineTo(xPerIndex + 130, y = y) //continue the path line at the end
                     }
                     if (index == 0) {
+                        previousTemp = y //fixes controlPoint1.y skip first index set
                         path.moveTo(0f, y)
                         path.lineTo(-130f, y = y) //start infinite line
                         path.moveTo(0f, y)
                         //draw vertical on first point
                         drawLine(
-                            color = Color.Red.copy(alpha = 0.5f),
+                            color = verticalLineColor,
                             start = Offset(x = 0f, y),
                             end = Offset(x = 0f, y = y + (height - y)),
                             strokeWidth = 3f,
@@ -124,106 +87,39 @@ fun HourlyGraph(modifier: Modifier = Modifier, data: List<Hourly>) {
                             ),
                         )
                     } else {
-//                        path.cubicTo(
-//                            x1 = controlPoints1.x,
-//                            y1 = controlPoints1.y,
-//                            x2 = controlPoints2.x,
-//                            y2 = controlPoints2.y,
-//                            x3 = xPerIndex,
-//                            y3 = y
-//                        )
-                        path.lineTo(
-                            x = xPerIndex,
-                            y = y
-                        )
-                        //vertical lines
-//                        drawLine(
-//                            color = Color.Red.copy(alpha = 0.5f),
-//                            start = Offset(x = xPerIndex, y),
-//                            end = Offset(x = xPerIndex, y = y+(height-y)),
-//                            strokeWidth = 3f,
-//                            pathEffect = PathEffect.dashPathEffect(
-//                                floatArrayOf(10f, 10f),
-//                                phase = 0f
-//                            ),
-//                        )
-                        drawCircle(Color.Black, radius = 10f, center = Offset(xPerIndex, y))
-                        drawPath(
-                            path = path,
-                            brush = Brush.verticalGradient(
-                                colors = listOf(
-                                    Color.Red,
-                                    Color.Yellow,
-                                    Color.Green,
-                                )
-                            ),
-                            style = Stroke(width = 5f)
-                        )
                         previousTemp = y
+                        path.cubicTo(
+                            x1 = controlPoints1.x,
+                            y1 = controlPoints1.y,
+                            x2 = controlPoints2.x,
+                            y2 = controlPoints2.y,
+                            x3 = xPerIndex,
+                            y3 = y
+                        )
                     }
                 }
+                drawPath(
+                    path = path,
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Red,
+                            Color.Yellow,
+                            Color.Green,
+                        )
+                    ),
+                    style = Stroke(width = 5f)
+                )
             }
         })
-}
-
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-private fun Ychart() {
-    val sampleData = List(6) { it }
-    val points = HourlyStaticData.mapIndexed { index, hourly ->
-        Point(index.toFloat(), hourly.temp.toFloat())
-    }
-//    val points = sampleData.mapIndexed { index, number ->
-//        Point(number.toFloat(), index.toFloat())
-//    }
-    val steps = HourlyStaticData.size
-    WeatherTheme {
-        val xAxisData = AxisData.Builder()
-            .axisStepSize(100.dp)
-            .steps(HourlyStaticData.size - 1)
-            .labelData { i -> i.toString() }
-            .labelAndAxisLinePadding(15.dp)
-            .backgroundColor(MaterialTheme.colorScheme.surface)
-            .axisLabelColor(MaterialTheme.colorScheme.onSurface)
-            .build()
-
-        val yAxisData = AxisData.Builder()
-            .steps(HourlyStaticData.maxOf { it.temp }.toInt())
-            .labelAndAxisLinePadding(20.dp)
-            .axisLineColor(MaterialTheme.colorScheme.onSurface)
-            .axisLabelColor(MaterialTheme.colorScheme.onSurface)
-            .backgroundColor(MaterialTheme.colorScheme.surface)
-//            .labelData { i ->
-//                (i).toString()
-//            }
-            .build()
-        val linechart = LineChartData(
-            linePlotData = LinePlotData(
-                lines = listOf(
-                    Line(
-                        dataPoints = points,
-                        lineStyle = LineStyle(color = MaterialTheme.colorScheme.onSurface),
-                        intersectionPoint = IntersectionPoint(color = MaterialTheme.colorScheme.onSurface),
-                        selectionHighlightPoint = SelectionHighlightPoint(),
-                        shadowUnderLine = ShadowUnderLine(),
-                        selectionHighlightPopUp = SelectionHighlightPopUp()
-                    )
-                ),
-            ),
-            xAxisData = xAxisData,
-            yAxisData = yAxisData,
-//        gridLines = GridLines(),
-            backgroundColor = MaterialTheme.colorScheme.surface
-        )
-        LineChart(modifier = Modifier.aspectRatio(16 / 9f), lineChartData = linechart)
-    }
 }
 
 @Preview
 @Composable
 private fun HourlyGraphPreview() {
     HourlyGraph(
-        modifier = Modifier,
+        modifier = Modifier
+            .padding(64.dp)
+            .aspectRatio(16 / 9f),
         data = HourlyStaticData
     )
 }
