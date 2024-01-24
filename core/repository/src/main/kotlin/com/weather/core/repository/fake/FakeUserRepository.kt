73790b0d3342db00986d28616415d3e0913a17cc
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-01-24 14:38:32 +0330

    removed favorite city coordinate return type for fake repo

--- core/repository/src/main/kotlin/com/weather/core/repository/fake/FakeUserRepository.kt
+++ core/repository/src/main/kotlin/com/weather/core/repository/fake/FakeUserRepository.kt
@@ -2,22 +2,15 @@ package com.weather.core.repository.fake
 
 import com.weather.core.repository.UserRepository
 import com.weather.core.repository.fake.data.listOfWeatherDataTest
-import com.weather.model.Coordinate
 import com.weather.model.TemperatureUnits
 import com.weather.model.WindSpeedUnits
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class FakeUserRepository : UserRepository {
-    override fun getFavoriteCityCoordinate(): Flow<Coordinate?> = flow {
-        val coordinate = listOfWeatherDataTest.random().let {
-            Coordinate(
-                it.coordinates.name,
-                it.coordinates.lat.toString(),
-                it.coordinates.lon.toString()
-            )
-        }
-        emit(coordinate)
+    override fun getFavoriteCityCoordinate(): Flow<String?> = flow {
+        val cityName = listOfWeatherDataTest.random().coordinates.name
+        emit(cityName)
     }
 
     override fun getTemperatureUnitSetting(): Flow<TemperatureUnits?> = flow {
