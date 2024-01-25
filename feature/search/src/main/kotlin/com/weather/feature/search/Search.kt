e56f7891ca41d7cda07deb48c6c8a2173e88b6e9
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-01-25 14:07:23 +0330

    added system status bar padding to search screen

--- feature/search/src/main/kotlin/com/weather/feature/search/Search.kt
+++ feature/search/src/main/kotlin/com/weather/feature/search/Search.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -83,6 +84,7 @@ fun SearchScreen(
             .background(
                 color = MaterialTheme.colorScheme.background
             )
+            .statusBarsPadding()
     ) {
         Column(
             modifier = Modifier
