95c8e7e2d4e4dca2fce63ca407488b50c5a60996
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-02-14 10:21:12 +0330

    cleanup sync module

--- sync/work/src/main/kotlin/com/weather/sync/work/WeatherWorkerFactory.kt
+++ sync/work/src/main/kotlin/com/weather/sync/work/WeatherWorkerFactory.kt
@@ -12,17 +12,18 @@ import javax.inject.Singleton
 
 @Singleton
 class WeatherWorkerFactory @Inject constructor(
-    private val repository : WeatherRepository
-): WorkerFactory() {
+    private val repository: WeatherRepository,
+) : WorkerFactory() {
 
     override fun createWorker(
         appContext: Context,
         workerClassName: String,
         workerParameters: WorkerParameters,
     ): ListenableWorker? {
-        return when(workerClassName){
+        return when (workerClassName) {
             FetchRemoteWeatherWorker::class.java.name ->
-                FetchRemoteWeatherWorker(context = appContext,workerParameters,repository)
+                FetchRemoteWeatherWorker(context = appContext, workerParameters, repository)
+
             else -> null
         }
     }
