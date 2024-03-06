4b0e61de544f29da81e7e6e7c6f107274fe17778
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-03-06 15:33:59 +0330

    match the text size with lines using pixels and then converting them to sp and dp

--- feature/forecast/src/main/kotlin/com/weather/feature/forecast/widgets/WindWidget.kt
+++ feature/forecast/src/main/kotlin/com/weather/feature/forecast/widgets/WindWidget.kt
@@ -1,61 +1,104 @@
 package com.weather.feature.forecast.widgets
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Air
-import androidx.compose.material.icons.outlined.ArrowUpward
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.weather.core.design.components.WeatherSquareWidget
+import kotlin.math.cos
+import kotlin.math.sin
 
 @Composable
 internal fun WindWidget(
     modifier: Modifier = Modifier,
 ) {
     WeatherSquareWidget(modifier, icon = Icons.Outlined.Air, title = "Wind") {
-        WindDirection(modifier = Modifier.padding(32.dp), windDirection = 90)
+        WindDirectionGraph(windDirection = 90)
     }
 }
 
 @Composable
-internal fun WindDirection(modifier: Modifier = Modifier, windDirection: Int) {
-    Box(modifier = modifier) {
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            drawCircle(
+internal fun WindDirectionGraph(modifier: Modifier = Modifier, windDirection: Int) {
+    val text = rememberTextMeasurer()
+    Canvas(
+        modifier = modifier
+            .aspectRatio(1f)
+            .padding(16.dp)
+    ) {
+        val width = size.width
+        val height = size.height
+        val halfWidth = width / 2
+        val inderRadius = halfWidth.times(0.85f)
+        val outerRadius = halfWidth.times(1.0f)
+        val letters = listOf("E", "N", "W", "S")
+        letters.forEachIndexed { index, letter ->
+            val deg = index * 90.0
+            val rad = Math.toRadians(deg)
+
+            val startX = (inderRadius * cos(rad)).plus(halfWidth).toFloat()
+            val endX = (outerRadius * cos(rad)).plus(halfWidth).toFloat()
+
+            val startY = (inderRadius * -sin(rad)).plus(halfWidth).toFloat()
+            val endY = (outerRadius * -sin(rad)).plus(halfWidth).toFloat()
+
+            drawLine(
                 color = Color.White,
-                radius = size.width / 2,
-                style = Stroke(width = size.width.times(0.01f))
+                start = Offset(startX, startY),
+                end = Offset(endX, endY),
+                strokeWidth = 5f
+            )
+
+            val textLayoutResult = text.measure(
+                text = letter,
+                style = TextStyle(fontSize = halfWidth.times(0.25f).toSp())
+            )
+
+            val textX = when (letter) {
+                "E" -> startX - textLayoutResult.size.width.div(0.75f)
+                "W" -> startX + textLayoutResult.size.width.div(3)
+                else -> startX - textLayoutResult.size.width.div(2)
+
+            }
+            val textY = when (letter) {
+                "E" -> startY - textLayoutResult.size.height.div(2)
+                "N" -> startY
+                "S" -> startY - textLayoutResult.size.height
+                else -> startY - textLayoutResult.size.height.div(2)
+
+            }
+            val textColor = Color.White.copy(alpha = 0.75f)
+
+            drawText(
+                textLayoutResult,
+                color = textColor,
+                topLeft = Offset(
+                    x = textX,
+                    y = textY
+                )
             )
         }
-        Icon(
-            imageVector = Icons.Outlined.ArrowUpward,
-            contentDescription = "wind direction arrow icon",
-            modifier = Modifier
-                .align(Alignment.Center)
-                .size(64.dp)
-                .graphicsLayer {
-                    rotationZ = windDirection.minus(180f)
-                },
-        )
     }
+//    Icon(
+//        imageVector = Icons.Outlined.ArrowUpward,
+//        contentDescription = "wind direction arrow icon",
+//        modifier = Modifier
+//            .align(Alignment.Center)
+//            .size(64.dp)
+//            .graphicsLayer {
+//                rotationZ = windDirection.minus(180f)
+//            },
+//    )
 }
 
 @Preview(backgroundColor = 0xFF255BFF, showBackground = false)
