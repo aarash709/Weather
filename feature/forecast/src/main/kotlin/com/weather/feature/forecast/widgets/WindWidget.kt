4b8f5c59764a917829ba278f29114564356d5aef
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-03-09 12:13:32 +0330

    code cleanup

--- feature/forecast/src/main/kotlin/com/weather/feature/forecast/widgets/WindWidget.kt
+++ feature/forecast/src/main/kotlin/com/weather/feature/forecast/widgets/WindWidget.kt
@@ -9,12 +9,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.text.TextMeasurer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.weather.core.design.components.WeatherSquareWidget
 import kotlin.math.cos
 import kotlin.math.sin
@@ -30,7 +31,7 @@ internal fun WindWidget(
 
 @Composable
 internal fun WindDirectionGraph(modifier: Modifier = Modifier, windDirection: Int) {
-    val text = rememberTextMeasurer()
+    val textMeasurer = rememberTextMeasurer()
     Canvas(
         modifier = modifier
             .aspectRatio(1f)
@@ -39,55 +40,11 @@ internal fun WindDirectionGraph(modifier: Modifier = Modifier, windDirection: In
         val width = size.width
         val height = size.height
         val halfWidth = width / 2
-        val inderRadius = halfWidth.times(0.85f)
+        val innerRadius = halfWidth.times(0.85f)
         val outerRadius = halfWidth.times(1.0f)
         val letters = listOf("E", "N", "W", "S")
-        letters.forEachIndexed { index, letter ->
-            val deg = index * 90.0
-            val rad = Math.toRadians(deg)
-
-            val startX = (inderRadius * cos(rad)).plus(halfWidth).toFloat()
-            val endX = (outerRadius * cos(rad)).plus(halfWidth).toFloat()
-
-            val startY = (inderRadius * -sin(rad)).plus(halfWidth).toFloat()
-            val endY = (outerRadius * -sin(rad)).plus(halfWidth).toFloat()
-
-            drawLine(
-                color = Color.White,
-                start = Offset(startX, startY),
-                end = Offset(endX, endY),
-                strokeWidth = 5f
-            )
-
-            val textLayoutResult = text.measure(
-                text = letter,
-                style = TextStyle(fontSize = halfWidth.times(0.25f).toSp())
-            )
-
-            val textX = when (letter) {
-                "E" -> startX - textLayoutResult.size.width.div(0.75f)
-                "W" -> startX + textLayoutResult.size.width.div(3)
-                else -> startX - textLayoutResult.size.width.div(2)
-
-            }
-            val textY = when (letter) {
-                "E" -> startY - textLayoutResult.size.height.div(2)
-                "N" -> startY
-                "S" -> startY - textLayoutResult.size.height
-                else -> startY - textLayoutResult.size.height.div(2)
-
-            }
-            val textColor = Color.White.copy(alpha = 0.75f)
-
-            drawText(
-                textLayoutResult,
-                color = textColor,
-                topLeft = Offset(
-                    x = textX,
-                    y = textY
-                )
-            )
-        }
+        drawLines(innerRadius, outerRadius, width)
+        drawLetters(letters, textMeasurer, innerRadius, width)
     }
 //    Icon(
 //        imageVector = Icons.Outlined.ArrowUpward,
@@ -99,6 +56,77 @@ internal fun WindDirectionGraph(modifier: Modifier = Modifier, windDirection: In
 //                rotationZ = windDirection.minus(180f)
 //            },
 //    )
+}
+
+private fun DrawScope.drawLines(
+    innerRadius: Float,
+    outerRadius: Float,
+    width: Float,
+    lineCount: Int = 72,
+    offsetDeg: Int = 5,
+) {
+    val halfWidth = width / 2
+    (0..<lineCount).forEach { index ->
+        val rad = (index.toDouble() * offsetDeg)
+        val lineRad = Math.toRadians(rad)
+        val lineColor = if (index % 18 == 0) Color.White else Color.White.copy(alpha = 0.2f)
+        val startLinesX = (innerRadius * cos(lineRad)).plus(halfWidth).toFloat()
+        val endLinesX = (outerRadius * cos(lineRad)).plus(halfWidth).toFloat()
+
+        val startLinesY = (innerRadius * -sin(lineRad)).plus(halfWidth).toFloat()
+        val endLinesY = (outerRadius * -sin(lineRad)).plus(halfWidth).toFloat()
+        drawLine(
+            color = lineColor,
+            start = Offset(startLinesX, startLinesY),
+            end = Offset(endLinesX, endLinesY),
+            strokeWidth = 5f
+        )
+    }
+}
+
+private fun DrawScope.drawLetters(
+    letters: List<String>,
+    textMeasurer: TextMeasurer,
+    inderRadius: Float,
+    width: Float,
+) {
+    val textColor = Color.White.copy(alpha = 0.75f)
+    letters.forEachIndexed { index, letter ->
+        val deg = index * 90.0
+        val rad = Math.toRadians(deg)
+        val halfWidth = width / 2
+
+        val startX = (inderRadius * cos(rad)).plus(halfWidth).toFloat()
+        val startY = (inderRadius * -sin(rad)).plus(halfWidth).toFloat()
+
+        val textLayoutResult = textMeasurer.measure(
+            text = letter,
+            style = TextStyle(fontSize = halfWidth.times(0.25f).toSp())
+        )
+
+        val textX = when (letter) {
+            "E" -> startX - textLayoutResult.size.width.div(0.75f)
+            "W" -> startX + textLayoutResult.size.width.div(3)
+            else -> startX - textLayoutResult.size.width.div(2)
+
+        }
+        val textY = when (letter) {
+            "E" -> startY - textLayoutResult.size.height.div(2)
+            "N" -> startY
+            "S" -> startY - textLayoutResult.size.height
+            else -> startY - textLayoutResult.size.height.div(2)
+
+        }
+
+        drawText(
+            textLayoutResult,
+            color = textColor,
+            topLeft = Offset(
+                x = textX,
+                y = textY
+            )
+        )
+    }
 }
 
 @Preview(backgroundColor = 0xFF255BFF, showBackground = false)
