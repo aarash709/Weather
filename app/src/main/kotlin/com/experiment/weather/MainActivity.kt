1da5e732264ffccc256e26b14a9641b6969c6fb6
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-03-22 20:44:15 +0330

    refactor

--- app/src/main/kotlin/com/experiment/weather/MainActivity.kt
+++ app/src/main/kotlin/com/experiment/weather/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -30,14 +31,16 @@ import timber.log.Timber
 class MainActivity : ComponentActivity() {
 
     private val mainViewModel: MainViewModel by viewModels()
-    private var hasInternet by mutableStateOf(false)
-    private var isDatabaseEmpty by mutableStateOf(true)
-    private var isDataLoaded by mutableStateOf(false)
 
     @FlowPreview
     @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var hasInternet by mutableStateOf(false)
+        var isDatabaseEmpty by mutableStateOf(true)
+        var isDataLoaded by mutableStateOf(false)
+
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 mainViewModel.hasInternet.collect {
@@ -49,6 +52,7 @@ class MainActivity : ComponentActivity() {
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 mainViewModel.isDataLoaded.collect {
+                    Timber.e("dataloaded:$it")
                     isDataLoaded = it
                 }
 
@@ -56,15 +60,17 @@ class MainActivity : ComponentActivity() {
         }
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                mainViewModel.dataBaseIsEmpty.collect {
+                mainViewModel.dataBaseIsEmpty.onEach { isDatabaseEmpty = it }.collect {
                     Timber.e("database is empty?:$it")
-                    isDatabaseEmpty = it
+
                 }
             }
         }
+
         val splashScreen = installSplashScreen()
         splashScreen.setKeepOnScreenCondition {
-            !isDatabaseEmpty && isDataLoaded
+            //keep splash screen on-screen until we have loaded our data and
+            !isDataLoaded
         }
 
         enableEdgeToEdge()
