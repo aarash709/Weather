9a5ea4dc6ac095ce3bc81b4d4eac7bac958bde78
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-04-12 13:19:34 +0330

    cleanup code

--- feature/forecast/src/main/kotlin/com/weather/feature/forecast/widgets/SunWidget.kt
+++ feature/forecast/src/main/kotlin/com/weather/feature/forecast/widgets/SunWidget.kt
@@ -18,17 +18,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.CompositingStrategy
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.asAndroidPath
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.weather.core.design.components.WeatherSquareWidget
-import kotlin.math.cos
-import kotlin.math.sin
 
 @Composable
 fun SunWidget(sunrise: Int, sunset: Int, currentTime: Int, modifier: Modifier = Modifier) {
@@ -64,13 +60,6 @@ private fun SunGraph(
             val sunsetColor = Color.Red.copy(green = 0.3f)
             val darkBlue = Color(5, 20, 50)
             val daylightColor = Color(244, 250, 127)
-            val daylightBrush = Brush.horizontalGradient(
-                0.0f to sunsetColor,
-                0.05f to daylightColor,
-                0.95f to daylightColor,
-                1.0f to sunsetColor
-            )
-            val nightColor = Brush.horizontalGradient(listOf(darkBlue, darkBlue))
 
             val timeRange = sunset.minus(sunrise)
 
@@ -80,14 +69,6 @@ private fun SunGraph(
                 .div(timeRange)
 
             onDrawBehind {
-                drawLine(
-                    Color.White.copy(alpha = 0.5f),
-                    start = Offset(
-                        width - (width.times(1.25f)),
-                        height / 2
-                    ),
-                    end = Offset(width.times(1.25f), height / 2)
-                )
                 val path = calculatePath()
                 val pathPosition = FloatArray(2)
                 val pathTangent = FloatArray(2)
@@ -95,22 +76,28 @@ private fun SunGraph(
                 val length = measure.length
                 measure.getPosTan(length * progress, pathPosition, pathTangent)
                 val brush = Brush.horizontalGradient(
-                    0.05f to darkBlue,
-                    0.15f to sunsetColor,
-                    0.2f to daylightColor,
-                    0.8f to daylightColor,
-                    0.85f to sunsetColor,
-                    0.95f to darkBlue
+                    0.0f to darkBlue,
+                    0.10f to sunsetColor,
+                    0.15f to daylightColor,
+                    0.85f to daylightColor,
+                    0.90f to sunsetColor,
+                    1.0f to darkBlue
                 )
                 val indicatorBoarderOffset = circleSize / 32
+                drawLine(
+                    Color.White.copy(alpha = 0.5f),
+                    start = Offset(
+                        width - (width.times(1.25f)),
+                        height / 1.20f
+                    ),
+                    end = Offset(width.times(1.25f), height / 1.20f)
+                )
                 drawSundial(
                     path = path,
                     brush = brush
                 )
                 drawCircleIndicator(
-                    brush = if (currentTime > sunset)
-                        nightColor
-                    else daylightBrush,
+                    brush = brush,
                     radius = circleSize,
                     boarderRadius = circleSize * indicatorBoarderOffset,
                     position = pathPosition,
@@ -181,9 +168,9 @@ private fun DrawScope.drawCircleIndicator(
 @Preview
 @Composable
 private fun UVPreview() {
-    val position = 50
+    val position = 55
     FlowRow(maxItemsInEachRow = 2, horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-        SunWidget(modifier = Modifier.weight(1f), sunrise = 0, sunset = 100, currentTime = position)
-        SunWidget(modifier = Modifier.weight(1f), sunrise = 0, sunset = 100, currentTime = position)
+        SunWidget(modifier = Modifier.weight(1f), sunrise = 20, sunset = 90, currentTime = position)
+        SunWidget(modifier = Modifier.weight(1f), sunrise = 20, sunset = 90, currentTime = position)
     }
 }
