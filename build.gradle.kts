9ed36f5eecbcbf7318ff072dae6c5b8c03945845
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-05-21 22:47:20 +0330

    kotlin 2.0.0 🎈

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.secrets) apply false
-    alias(libs.plugins.andoirdGradle) apply false
+    alias(libs.plugins.android.application) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlinGradle) apply false
     alias(libs.plugins.kotlinSerialization) apply false
-    alias(libs.plugins.com.android.library) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlinx.ksp) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.kotlinter) apply false
