cc17ee05a09df105d4615480bdb0550d76174c5a
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-06-20 21:10:43 +0330

    using basic dialog

--- feature/settings/src/main/kotlin/com/weather/feature/settings/SettingsGroup.kt
+++ feature/settings/src/main/kotlin/com/weather/feature/settings/SettingsGroup.kt
@@ -14,7 +14,7 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -133,10 +133,10 @@ internal fun TemperatureOptions(
     onDismissRequest: () -> Unit,
 ) {
     if (shouldShowOptions) {
-        AlertDialog(
+        BasicAlertDialog(
             onDismissRequest = { onDismissRequest() },
             modifier = Modifier
-                .windowInsetsPadding(dialogInsets),
+                .windowInsetsPadding(dialogInsets)
         ) {
             Surface(
                 modifier = modifier,
@@ -175,10 +175,10 @@ fun WindSpeedOptions(
     onDismissRequest: () -> Unit,
 ) {
     if (shouldShowOptions) {
-        AlertDialog(
+        BasicAlertDialog(
             onDismissRequest = { onDismissRequest() },
             modifier = Modifier
-                .windowInsetsPadding(dialogInsets),
+                .windowInsetsPadding(dialogInsets)
         ) {
             Surface(
                 modifier = modifier,
