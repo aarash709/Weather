a94c3f6cff6009a78ee0459d8f9f01fc9476eaee
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-06-24 14:55:48 +0330

    disable scrolling so the draggable modifier works

--- feature/forecast/src/main/kotlin/com/weather/feature/forecast/WeatherForecastScreen.kt
+++ feature/forecast/src/main/kotlin/com/weather/feature/forecast/WeatherForecastScreen.kt
@@ -14,26 +14,20 @@ import androidx.compose.foundation.gestures.AnchoredDraggableState
 import androidx.compose.foundation.gestures.DraggableAnchors
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.anchoredDraggable
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Surface
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
@@ -43,7 +37,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
@@ -67,7 +60,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -99,12 +91,10 @@ import com.weather.model.WeatherData
 import com.weather.model.WindSpeedUnits
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
-import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
 
 enum class Anchors {
@@ -180,7 +170,7 @@ fun WeatherForecastScreen(
             )
         })
     val scrollState = rememberScrollState()
-    var isScrollEnabled by rememberSaveable{
+    var isScrollEnabled by rememberSaveable {
         mutableStateOf(false)
     }
 //    val scrollProgress by remember(scrollState.value) {
@@ -188,54 +178,25 @@ fun WeatherForecastScreen(
 //            (scrollState.value.toFloat() / scrollState.maxValue.toFloat()).times(100)
 //        }
 //    }
-    var returnToClosedAnchor by remember {
-        mutableStateOf(false)
-    }
-
-    val anchors = DraggableAnchors {
-        Anchors.Closed at 0f
-        Anchors.OPEN at 0f
-    }
     val draggableState = remember {
         AnchoredDraggableState(
             initialValue = Anchors.Closed,
-//                anchors = anchors,
             positionalThreshold = { totalDistance -> totalDistance * 0.5f },
             animationSpec = spring(),
             velocityThreshold = { with(density) { 100.dp.toPx() } }
         )
     }
-    LaunchedEffect(key1 = returnToClosedAnchor) {
-        if (returnToClosedAnchor) {
-            draggableState.anchoredDrag(Anchors.Closed) { anchors, targetValue ->
-
-            }
-        }
-    }
-    SideEffect {
-        isScrollEnabled = draggableState.currentValue == Anchors.OPEN && scrollState.value == 0
+    LaunchedEffect(key1 = draggableState.currentValue) {
+        isScrollEnabled = draggableState.currentValue == Anchors.OPEN
     }
     val connection = remember {
         object : NestedScrollConnection {
             override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-//                Timber.e("avalible: ${available.y}")
-//                Timber.e("draggg: ${draggableState.currentValue.name}")
-//                val scrollableConsumed = scrollState.value
-//                Timber.e("scrCons: ${scrollableConsumed}")
-//                //
-//                if (available.y > 0f && draggableState.currentValue == Anchors.OPEN) {
-//                    draggableState.dispatchRawDelta(-available.y)
-//                    scrollState.dispatchRawDelta(-available.y)
-//                    if (scrollState.value == 0) {
-//                        draggableState.dispatchRawDelta(-available.y)
-//                    }
-//                    return available
-//                }
-//                if (available.y < 0f && draggableState.currentValue == Anchors.Closed) {
-//                    draggableState.dispatchRawDelta(available.y)
-//                    return Offset.Zero
-//                }
-//                    scrollState.dispatchRawDelta(delta)
+                //disable scrolling if Anchor is Anchor.OPEN and scroll position is 0(at the beginning)
+                //and scrolling down
+                if (available.y > 0f && draggableState.currentValue == Anchors.OPEN && scrollState.value ==0) {
+                    isScrollEnabled = false
+                }
                 return super.onPreScroll(available, source)
             }
         }
