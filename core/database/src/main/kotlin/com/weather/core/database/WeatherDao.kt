35ae56ba3b1fec95dbf4bdbe57a7f3b13d697e49
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2025-04-14 16:14:54 +0330

    fix database error

--- core/database/src/main/kotlin/com/weather/core/database/WeatherDao.kt
+++ core/database/src/main/kotlin/com/weather/core/database/WeatherDao.kt
@@ -1,8 +1,9 @@
 package com.weather.core.database
 
-import androidx.room.*
-import androidx.room.OnConflictStrategy.Companion.REPLACE
-import com.weather.core.database.entities.geoSearch.GeoSearchItemEntity
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Upsert
 import com.weather.core.database.entities.meteo.CurrentEntity
 import com.weather.core.database.entities.meteo.DailyEntity
 import com.weather.core.database.entities.meteo.HourlyEntity
@@ -56,63 +57,20 @@ interface WeatherDao {
 	/**
 	 * method to store data.
 	 */
-//	@Upsert
-//	suspend fun insertOneCall(oneCall: OneCallEntity)
-
-//	@Upsert
-//	suspend fun insertOneCalls(oneCalls: List<OneCallEntity>)
-
-//	@Upsert
-//	suspend fun insertOneCallCurrent(current: com.weather.core.database.entities.onecall.CurrentEntity)
-//
-//	@Upsert
-//	suspend fun insertOneCallCurrentWeather(weather: List<CurrentWeatherEntity>)
-//
-//	@Upsert
-//	suspend fun insertDaily(daily: List<com.weather.core.database.entities.onecall.DailyEntity>)
-//
-//	@Upsert
-//	suspend fun insertHourly(hourly: List<OneCallHourlyEntity>)
 
 	@Query("SELECT count(*) FROM weather_location WHERE cityName = :cityName")
 	fun checkIfCityExists(cityName: String): Int
 
-//	@Query("SELECT * FROM one_call")
-//	fun getAllOneCall(): List<OneCallEntity>
-//
-//	@Query("SELECT * FROM one_call WHERE cityName = :cityName")
-//	fun getOneCallByCityName(cityName: String): OneCallEntity
-//
-//	@Transaction
-//	@Query("SELECT * FROM one_call WHERE cityName = :cityName")
-//	fun getOneCallAndCurrentByCityName(cityName: String): Flow<OneCallAndCurrent>
-//
-//	@Transaction
-//	@Query("SELECT * FROM one_call_current WHERE cityName = :cityName")
-//	fun getCurrentWithWeatherByCityName(cityName: String): Flow<CurrentWithWeather>
-//
 	@Transaction
 	@Query("SELECT * FROM weather_location")
 	fun getAllWeatherAndCurrent(): Flow<List<WeatherAndCurrent>>
 
 	//delete
-
 	@Query("delete from weather_location where cityName= :cityName")
 	fun deleteOneWeather(cityName: String)
 
 	@Query("DELETE FROM weather_location WHERE cityName in (:cityName)")
 	suspend fun deleteMultipleWeather(cityName: List<String>)
-//
-//	@Query("select * from one_call_daily where cityName= :cityName")
-//	fun getDailyByCityName(cityName: String): Flow<List<DailyEntity>>
-
-	// geo
-	@Insert(onConflict = REPLACE)
-	suspend fun insertGeoSearch(geoSearch: GeoSearchItemEntity)
-
-//	@Transaction
-//	@Query("select * from one_call")
-//	fun getAllOneCallWithCurrentAndDailyAndHourly(): Flow<List<OneCallWithCurrentAndDailyAndHourly>> //experimental
 
 	/**
 	 * Delete old data based on timeStamp of the network
@@ -141,16 +99,5 @@ interface WeatherDao {
 		insertDaily(daily)
 		insertHourly(hourly)
 	}
-
-//	@Transaction
-//	suspend fun reorderOneCallWeatherData(
-//		from: OneCallEntity,
-//		to: OneCallEntity,
-//		fromIndex: Int,
-//		toIndex: Int,
-//	) {
-//		insertOneCall(from.copy(orderIndex = toIndex))
-//		insertOneCall(to.copy(orderIndex = fromIndex))
-//	}
 
 }
